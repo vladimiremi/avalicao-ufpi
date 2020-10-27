d39b0050d6a426cb482e53b72ea0c0af75104896
vladimiremi <vladimiremi2014@gmail.com>
2020-10-27 08:45:05 -0300

    add: function Rondom Int

--- content.js
+++ content.js
@@ -5,6 +5,12 @@ const button = document.createElement("button"); //cria e armazena meu botão na
 button.innerHTML = "Avaliar Todos"; //coloca o contente no html
 button.classList.add("avalia"); //coloca uma class
 
+function getRandomInt(min, max) { //Gera um inteiro aleatório dentro de um intervalo.
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
 button.addEventListener("click", (e)=>{
 
     e.preventDefault();
@@ -12,11 +18,12 @@ button.addEventListener("click", (e)=>{
     var selects = document.querySelectorAll('select'); //Pega todos os elementos select e coloca em um Array
 
     selects.forEach((select) => {
-        select[1].selected = true;
+        select[getRandomInt(1, 3)].selected = true;
+
     })
 
 })
 
 header.appendChild(button);
 
-console.log(button);
+
