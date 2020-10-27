01c38782d3e6052e9858bfb915d8c06d530995e3
vladimiremi <vladimiremi2014@gmail.com>
2020-10-27 08:25:10 -0300

    fix: refresh of button

--- content.js
+++ content.js
@@ -1,44 +1,21 @@
-
-
-const header = document.querySelector("#elgen-14");
+const header = document.querySelector(".ytab-strip-wrap");
 console.log(header);
 
-const button = document.createElement("button");
-button.innerHTML = "Avaliar Todos";
-button.classList.add("avalia");
+const button = document.createElement("button"); //cria e armazena meu botão na variável button
+button.innerHTML = "Avaliar Todos"; //coloca o contente no html
+button.classList.add("avalia"); //coloca uma class
 
-// button.addEventListener("click", ()=>{
+button.addEventListener("click", (e)=>{
 
-//     var select = document.querySelectorAll('select'); //Pega todos os elementos select e coloca em um Array
-
-//     for(i=0; i< select.length; i++){ //pega cada select e marca a opção desejada.
-
-//     let option = select[i].children; //Dá pra pegar todos os sub elementos option que estão no select específico
-//     option[1].selected = true; //Atribuo a nota onde 0 = "" | 1 = 5 | 2 = 4 | 3 = 3 | 4 = 2 | 5 = 1 |
-
-// };
-
-// })
-
-
-button.addEventListener("click", ()=>{
-
+    e.preventDefault();
+    
     var selects = document.querySelectorAll('select'); //Pega todos os elementos select e coloca em um Array
 
     selects.forEach((select) => {
         select[1].selected = true;
     })
 
-
-    // for(i=0; i< select.length; i++){ //pega cada select e marca a opção desejada.
-
-    // let option = select[i].children; //Dá pra pegar todos os sub elementos option que estão no select específico
-    // option[1].selected = true; //Atribuo a nota onde 0 = "" | 1 = 5 | 2 = 4 | 3 = 3 | 4 = 2 | 5 = 1 |
-
-// };
-
 })
-
 
 header.appendChild(button);
 
